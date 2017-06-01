b5fd04c22c9c120766e3ab273a9fb8a4fcf4a208
Nolwenn Lavielle <nolwenn.lavielle@gmail.com>
2017-06-01 11:26:41 +0200

    Add DocString

--- MI_FP_common.py
+++ MI_FP_common.py
@@ -2,11 +2,22 @@
 import pandas as pd
 
 def df_dtypes_object(dataframe):
+    """
+        Convert each field of dataframe as type object
+
+        dataframe - dataframe
+    """
     for col in dataframe.columns:
         dataframe[col] = dataframe[col].astype(object)
     return dataframe
 
 def complete_columns(dataframe, list_columns):
+    """
+        From a dataframe, complete selected columns with value in line before
+
+        dataframe    - dataframe
+        list_columns - list of columns
+    """
     for column in list_columns:
         dataframe[column] = pd.Series([dataframe.loc[0, column]]*len(dataframe))
     return dataframe
